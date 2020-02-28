ebe091b7a250ab6e1667e805097149b93d9dbbf2
Birendra Bikram <birendra.singh@velotio.com>
2020-02-28 11:59:26 +0530

    microfrontends using reactjs

---
+++ precache-manifest.337ef5cad4b38bf60a3f7c16e50f0eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf199c4a6257add71f6410923a9a50dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855da0671f6f4eaa5a03",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f0766cbf40cdc9bb4fef",
+    "url": "/static/js/2.bba3d0f3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.bba3d0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855da0671f6f4eaa5a03",
+    "url": "/static/js/main.3388e2c9.chunk.js"
+  },
+  {
+    "revision": "9a92d0b1ee797b5e1f22",
+    "url": "/static/js/runtime-main.2f75b410.js"
+  }
+]);
